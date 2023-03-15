79777d0a3d5eb3f03c8d19134ca12938b137fd2f
Alex Patient <apatient97@gmail.com>
2023-03-15 22:20:30 +0000

    updated modal

--- client/src/components/MothersDay/index.js
+++ client/src/components/MothersDay/index.js
@@ -13,20 +13,21 @@ function MothersDay({setMothersDay}) {
     }
 
   return (
-   <div className="flex">
-    <div className="md-popout">
-        <button onClick={closeModal} className='close-modal'>X</button>
-        <h1>Mothers' Day Flowers</h1>
-        <p>Shop seasonal for this Mothers' Day via our web shop</p>
-        <Link to='/shop'>
-            <button onClick={goToShop} className='buy-btn'>view options</button>
-        </Link>
-        <div className="modal-pic">
-
+   <>
+        <div className="flex">
+            <div className="md-popout">
+                <p onClick={closeModal} className='close-modal'>close</p>
+                <h1>Mothers' Day Flowers</h1>
+                <p>Shop seasonal for this Mothers' Day via our web shop</p>
+                <Link to='/shop'>
+                    <button onClick={goToShop} className='buy-btn'>view options</button>
+                </Link>
+                <div className="modal-pic"></div>
+            </div>
         </div>
-    
-    </div>
-   </div>
+        <div className="fader">
+        </div>
+   </>
     
    
   )
